04cb3fc79e2ba3d553bc5e5ea40079f5375b1ab9
gilvan <g.r.almeida@live.com>
2019-01-15 23:52:44 -0200

    Implementing pagination with skip and limit

--- common/model-router.ts
+++ common/model-router.ts
@@ -4,6 +4,7 @@ import {Router} from './router'
 
 export abstract class ModelRouter<D extends mongoose.Document> extends Router {
     basePath: string
+    pageSize: number = 1
 
     constructor(protected model: mongoose.Model<D>) {
         super()
@@ -24,10 +25,49 @@ export abstract class ModelRouter<D extends mongoose.Document> extends Router {
         }
     }
 
+    envelopeAll(documents: any[], options): any {
+        const resource: any = {
+            _links: {
+                self: `${options.url}`
+            },
+            items: documents
+        }
+
+        if(options.page && options.count && options.pageSize) {
+            if(options.page > 1) {
+                resource._links.previous = `${this.basePath}?_page=${options.page - 1}`
+            }
+
+            const remaining = options.count - (options.page * options.pageSize)
+
+            if(remaining > 0) {
+                resource._links.next = `${this.basePath}?_page=${options.page + 1}`
+            }
+        }
+
+        return resource
+    }
+
     findAll = (request, response, next) => {
+        let page = parseInt(request.query._page || 1)
+        page = page > 0 ? page : 1
+        const skip = (page - 1) * this.pageSize
+
         this.model
-            .find()
-            .then(this.renderAll(response, next))
+            .count({})
+            .exec()
+            .then(count => {
+                this.model
+                    .find()
+                    .skip(skip)
+                    .limit(this.pageSize)
+                    .then(this.renderAll(response, next, {
+                        page, 
+                        count, 
+                        pageSize: this.pageSize, 
+                        url: request.url
+                    }))
+            })
             .catch(next)
     }
 
